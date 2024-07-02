92a7832500dc9869f715f91ab7b941f6e7909fd2
Rinat_K <rinat.kurmakaev@outlook.com>
2024-07-02 14:13:47 +0300

    Change bd models

--- internal/models/worklogs.go
+++ internal/models/worklogs.go
@@ -5,16 +5,12 @@ import (
 )
 
 type Worklog struct {
-	WorklogID  int32     `json:"worklog_id"`
-	UserID     int32     `json:"user_id"`
-	TaskID     int32     `json:"task_id"`
-	StartTime  time.Time `json:"start_time"`
-	EndTime    time.Time `json:"end_time"`
-	HoursSpent int64     `json:"hours_spent"`
+	WorklogID   int32     `json:"worklog_id,omitempty"`
+	UserID      int32     `json:"user_id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description,omitempty"`
+	StartTime   time.Time `json:"start_time,omitempty"`
+	EndTime     time.Time `json:"end_time,omitempty"`
+	HoursSpent  int64     `json:"hours_spent,omitempty"`
 }
 
-type Task struct {
-	TaskID      int32  `json:"task_id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-}
